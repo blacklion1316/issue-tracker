1627f8650657601d115c91581b0a8d03803a59f5
blacklion1316 <kyp1396@gmail.com>
2024-03-30 21:49:56 -0500

    Add Radix UI themes and EasyMDE editor

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 function IssuePage() {
-    return <div>IssuePage</div>;
+    return (
+        <div>
+            <h1>Issue Page</h1>
+            <Link href="/issues/new">
+                <Button>Click me</Button>
+            </Link>
+        </div>
+    );
 }
 
 export default IssuePage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
+import "@radix-ui/themes/styles.css";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/app/components/NavBar";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Theme, ThemePanel } from "@radix-ui/themes";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -17,9 +20,12 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>
-                <NavBar />
-                {children}
+            <body className={inter.variable}>
+                <Theme>
+                    <NavBar />
+                    {children}
+                    {/* <ThemePanel /> */}
+                </Theme>
             </body>
         </html>
     );
