79f5898f032bd181d0005a8112f1706ece0d0c11
blacklion1316 <kyp1396@gmail.com>
2024-03-30 22:38:43 -0500

    Add spinner and disable submit button while submitting issue

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -15,6 +15,8 @@ import z from "zod";
 
 import ErrorMessage from "@/app/components/ErrorMessage";
 
+import Spinner from "@/app/components/spinner";
+
 type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
@@ -29,6 +31,7 @@ const NewIssuePage = () => {
     const router = useRouter();
 
     const [error, setError] = useState("");
+    const [isSubmitting, setIsSubmitting] = useState(false);
     return (
         <div>
             {error && (
@@ -40,9 +43,13 @@ const NewIssuePage = () => {
                 className="max-w-xl space-y-3"
                 onSubmit={handleSubmit(async (data) => {
                     try {
+                        setIsSubmitting(true);
                         await axios.post("/api/issues", data);
                         router.push("/issues");
-                    } catch (error) {}
+                    } catch (error) {
+                        setIsSubmitting(false);
+                        setError("An error occurred, please try again later");
+                    }
                 })}
             >
                 <TextField.Root
@@ -63,7 +70,9 @@ const NewIssuePage = () => {
                     <ErrorMessage>{errors.description.message}</ErrorMessage>
                 )}
 
-                <Button>Submit New Issue</Button>
+                <Button disabled={isSubmitting}>
+                    Submit New Issue {isSubmitting && <Spinner />}
+                </Button>
             </form>
         </div>
     );
